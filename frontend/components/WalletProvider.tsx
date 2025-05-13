a4f0d80f5b5702c59f9ce4549773c1c9ce003819
0xAnto <antonyranjith17@gmail.com>
2025-05-13 13:31:35 +0530

    enable evm wallets

--- frontend/components/WalletProvider.tsx
+++ frontend/components/WalletProvider.tsx
@@ -1,14 +1,14 @@
 import { PropsWithChildren } from "react";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { setupAutomaticSolanaWalletDerivation } from "@aptos-labs/derived-wallet-solana";
-// import { setupAutomaticEthereumWalletDerivation } from "@aptos-labs/derived-wallet-ethereum";
+import { setupAutomaticEthereumWalletDerivation } from "@aptos-labs/derived-wallet-ethereum";
 // Internal components
 import { useToast } from "@/components/ui/use-toast";
 // Internal constants
 import { APTOS_API_KEY, NETWORK } from "@/constants";
 
 setupAutomaticSolanaWalletDerivation({ defaultNetwork: NETWORK }); // this is the Aptos network your dapp is working with
-//  setupAutomaticEthereumWalletDerivation({ defaultNetwork: NETWORK });
+ setupAutomaticEthereumWalletDerivation({ defaultNetwork: NETWORK });
 
 export function WalletProvider({ children }: PropsWithChildren) {
   const { toast } = useToast();
